c26414fab1c7a3df9ff8913822e72fa3c5e64248
long2ice <long2ice@gmail.com>
2022-03-23 19:03:56 +0800

    feat: add ErrorHandler in example

--- examples/main.go
+++ examples/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -12,7 +13,22 @@ import (
 )
 
 func main() {
-	app := fibers.New(NewSwagger(), fiber.Config{})
+	app := fibers.New(NewSwagger(), fiber.Config{ErrorHandler: func(c *fiber.Ctx, err error) error {
+		code := fiber.StatusInternalServerError
+		if e, ok := err.(*fiber.Error); ok {
+			code = e.Code
+		}
+		if _, ok := err.(validator.ValidationErrors); ok {
+			code = fiber.StatusBadRequest
+		}
+		err = c.Status(code).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString("Internal Server Error")
+		}
+		return nil
+	}})
 	app.Use(
 		logger.New(),
 		recover.New(),
